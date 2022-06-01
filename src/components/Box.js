9d6257f3984d3264cb904e306e329b602e953a5b
Antonio <antonio.garciamn@gmail.com>
2022-06-01 17:22:53 -0300

    feat: api get modalities

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,16 +2,16 @@ import styled from 'styled-components';
 
 const Box = styled.section`
   height: 100%;
-  max-height: ${(props) => props.height || '550px'}; 
-  
+  max-height: ${(props) => props.height || '500px'};
+
   width: 100%;
   max-width: ${(props) => props.width || '1200px'};
-  
+
   overflow: hidden;
 
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: ${(props) => props.center || 'center'};
+  align-items: ${(props) => props.center || 'center'};
 
   @media (max-width: 600px) {
     border-radius: 0;
--- src/pages/Dashboard/Payment/PaymentScreen.js
+++ src/pages/Dashboard/Payment/PaymentScreen.js
@@ -2,18 +2,23 @@ import Title from '../../../components/Title';
 import useEnrollment from '../../../hooks/api/useEnrollment';
 import Box from '../../../components/Box';
 import GreyText from '../../../components/GreyText';
+import useModality from '../../../hooks/api/useModality';
 
 export default function PaymentScreen() {
   const { enrollment } = useEnrollment();
+  const { modalities, getModalityError } = useModality();
 
   return (
     <>
       <Title>Ingresso e pagamento </Title>
-      <Box>
+      <Box center={enrollment}>
         {enrollment ? (
-          'masss'
+          <GreyText>Primeiro, escolha sua modalidade de ingresso</GreyText>
+
         ) : (
-          <GreyText width="200">Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</GreyText>
+          <GreyText width="200">
+            Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso
+          </GreyText>
         )}
       </Box>
     </>
