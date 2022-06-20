f1e664d35c365f371ad45560e9b93d3f29037e67
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2022-06-20 12:37:02 -0300

    refactor: adjust activity status

--- src/pages/Dashboard/Activities/Activity.js
+++ src/pages/Dashboard/Activities/Activity.js
@@ -1,7 +1,7 @@
-import { IoLogInOutline, IoCloseCircleOutline } from 'react-icons/io5';
 import styled from 'styled-components';
 
 import { formatTime } from '../../../utils/timeFormat';
+import ActivityStatus from './ActivityStatus';
 
 export default function Activity({ activityInfo, isSelected, onClick }) {
   const {
@@ -18,28 +18,16 @@ export default function Activity({ activityInfo, isSelected, onClick }) {
         <Name>
           {name}
         </Name>
+
         <Time>
           {formatTime(+startAt, +duration)}
         </Time>
       </LeftSide>
 
-      <RightSide>
-        {capacity <= occupation ? (
-          <>
-            <IoCloseCircleOutline className="red-icon" />
-            <Number color={'red'}>
-              Esgotado
-            </Number>
-          </>
-        ) : (
-          <>
-            <IoLogInOutline className="green-icon" />
-            <Number color={'green'}>
-              {`${capacity - occupation} vagas`}
-            </Number>
-          </>
-        )}
-      </RightSide>
+      <ActivityStatus
+        isSelected={isSelected}
+        vacancy={capacity - occupation}
+      />
     </Container>
   );
 }
@@ -84,38 +72,4 @@ const Time = styled.p`
   color: #343434;
 
   margin-top: 7px;
-`;
-
-const RightSide = styled.div`
-  width: 64px;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  border-left: 1px solid #CFCFCF;
-
-  .green-icon {
-    font-size: 20px;
-    color: #078632;
-
-    margin-right: 5px;
-  }
-
-  .red-icon {
-    font-size: 20px;
-    color: #CC6666;
-
-    margin-right: 5px;
-  }
-`;
-
-const Number = styled.p`
-  font-family: 'Roboto';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 9px;
-  color: ${(props) => (props.color === 'green' ? '#078632' : '#CC6666')};
-  margin-top: 3px;
 `;
